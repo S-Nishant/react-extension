6d73080659ed2d9d2ae9f361d11903b124c2963d
S-Nishant <s.nishant@aitglobalinc.com>
2021-09-13 22:22:48 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainDashboard from './component/MainDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <MainDashboard></MainDashboard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MainDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MainDashboard.css";
+function MainDashboard() {
+    return (
+        <div>
+            <h1 className="red">
+            Dashboard
+            </h1>
+        </div>
+    )
+}
+
+export default MainDashboard
